d67ad63db8d1e1f96a33ffd9ea6e4f868c989449
Patrick <pkearney06@gmail.com>
2015-03-04 23:08:31 -0500

    Initial Commit

---
+++ glowfish/Glower.php
@@ -0,0 +1,106 @@
+<?php
+
+include(dirname(__FILE__) . '/frameworks/httpful.phar');
+require_once(dirname(__FILE__) . '/config/config.php');
+
+class Glower {
+	private $_sid = null;
+	private $_token = null;
+	
+	private $attributes			= array(
+		"set_name"				=> "default",
+	
+		"fail_if_mistakes"		=> false,
+		"delete_previous_data"	=> false,
+		"save_data"				=> false,
+		"stats"					=> false,
+		"hold"					=> false,
+		"update"				=> false,
+		
+		"max_number"			=> -1,
+		"accuracy"				=> false,
+		"group_max_number"		=> -1
+	);
+
+ 	public function __construct($sid, $token){
+	 	$this->_sid 		= $sid;
+	 	$this->_token 	= $token;
+ 	}
+ 	
+ 	public function train($data_set = array(), $response = array()){
+	 	$data = compact('data_set', 'response');
+	 	return $this->_request('train', $data);
+ 	}
+ 	
+ 	public function train_csv($data_set = "", $response = ""){
+	 	$attach = compact('data_set', 'response');
+	 	return $this->_request('train/csv', array(), $attach);
+ 	}
+ 	
+ 	public function predict($data_set = array(), $response = array()){
+	 	$data = compact('data_set', 'response');
+	 	return $this->_request('predict', $data);
+ 	}
+ 	
+ 	public function predict_csv($data_set = "", $response = ""){
+	 	$attach = compact('data_set', 'response');
+	 	return $this->_request('predict/csv', array(), $attach);
+ 	}
+ 	
+ 	public function cluster($data_set = array()){
+	 	$data = compact('data_set');
+	 	return $this->_request('cluster', $data);
+ 	}
+ 	
+ 	public function cluster_csv($data_set = ""){
+	 	$attach = compact('data_set');
+	 	return $this->_request('cluster/csv', array(), $attach);
+ 	}
+ 	
+ 	public function feature_select($data_set = array(), $response = array()){
+	 	$data = compact('data_set', 'response');
+	 	return $this->_request('feature_select', $data);
+ 	}
+ 	
+ 	public function feature_select_csv($data_set = "", $response = ""){
+	 	$attach = compact('data_set', 'response');
+	 	return $this->_request('feature_select/csv', array(), $attach);
+ 	}
+ 	
+ 	private function _request($endpoint, $data = array(), $attachments = null){
+ 		foreach($this->attributes as $attribute => $value){
+	 		$data[$attribute] = (!is_bool($value) ? $value : ($value ? "true" : "false"));
+ 		}
+ 		
+ 		//echo '<pre>';var_dump($attachments);echo '</pre>';
+ 	
+	 	$request = Httpful\Request::post(API_ENDPOINT . API_VERSION . '/' . $endpoint . '/')
+	 		->expectsJson()
+	 		//->sendsJson()
+	 		->authenticateWith($this->_sid, $this->_token)
+	 		->body($data);
+	 		
+	 	if($attachments){
+	 		$request->attach($attachments);
+	 	}
+	 	
+	 	try {
+	 		return $request->send();
+	 	} catch(Exception $e){
+	 		echo '<pre>';var_dump($e);echo '</pre>';
+	 	}
+ 	}
+ 	
+ 	public function __set($key, $value){
+	 	if(isset($this->attributes[$key])){
+		 	$this->attributes[$key] = $value;
+	 	}
+ 	}
+ 	
+ 	public function __get($key){
+	 	if(isset($this->attributes[$key])){
+		 	return $this->attributes[$key];
+	 	}
+	 	return null;
+ 	}
+}
---
+++ glowfish/config/config.php
@@ -0,0 +1,4 @@
+<?php
+
+define('API_ENDPOINT', 'https://api.glowfi.sh/');
+define('API_VERSION', 'v1');
